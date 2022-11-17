a64d126b0b821a54c70d54f2ae64b95c8708ef7d
Carlos A. Monterrosa <carlosjarguelles@icloud.com>
2022-11-16 21:52:29 -0500

    refactor: return ArrayList

--- src/main/java/process/ProcessService.java
+++ src/main/java/process/ProcessService.java
@@ -1,18 +1,18 @@
 package process;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class ProcessService {
-    public static List<Process> all() throws SecurityException, UnsupportedOperationException {
+    public static ArrayList<Process> all() throws SecurityException, UnsupportedOperationException {
         return all(null);
     }
-    public static List<Process> all(Integer limit) throws SecurityException, UnsupportedOperationException {
+
+    public static ArrayList<Process> all(Integer limit) throws SecurityException, UnsupportedOperationException {
         var ps = ProcessHandle.allProcesses();
         var processes = new ArrayList<Process>();
         for (ProcessHandle p : ps.toList()) {
             processes.add(new Process(p));
         }
-        return limit != null ? processes.subList(0, limit): processes;
+        return limit != null ? new ArrayList<>(processes.subList(0, limit)) : processes;
     }
 }
